fb68c337eb6d6e7e419777637a453aaa8b5d7d03
donowhy <donowhy11@gmail.com>
2018-10-15 18:47:01 -0700

    Added analysis mockup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { IonicStorageModule, Storage } from '@ionic/storage';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { ChartsModule } from 'ng2-charts';
 
 import { Items } from '../mocks/providers/items';
 import { Settings, User, Api } from '../providers';
@@ -39,6 +40,7 @@ export function provideSettings(storage: Storage) {
     MyApp
   ],
   imports: [
+    ChartsModule,
     BrowserModule,
     HttpClientModule,
     TranslateModule.forRoot({
--- src/pages/analysis/analysis.module.ts
+++ src/pages/analysis/analysis.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicPageModule } from 'ionic-angular';
+import { ChartsModule } from 'ng2-charts';
 
-import { SearchPage } from './search';
+import { SearchPage } from './analysis';
 
 @NgModule({
   declarations: [
     SearchPage,
   ],
   imports: [
+    ChartsModule,
     IonicPageModule.forChild(SearchPage),
     TranslateModule.forChild()
   ],
